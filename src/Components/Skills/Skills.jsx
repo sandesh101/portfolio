d1294e1af683b4c86ba20fa9ce62132a50723263
sandesh101 <rimal.sandesh11@gmail.com>
2025-06-23 07:49:14 +0545

    Skills Responsive

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -3,14 +3,21 @@ import { skills } from "../../constants/skillCardItems";
 
 const Skills = () => {
   return (
-    <section className="flex flex-col px-[13%] mt-10" id="skills">
-      <h1 className="text-6xl font-bold">Current Technologies</h1>
-      <p className="text-[#A0A0AA] mt-4">
+    <section
+      className="flex flex-col px-4 sm:px-10 lg:px-[13%] mt-10"
+      id="skills"
+    >
+      <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">
+        Current Technologies
+      </h1>
+
+      <p className="text-[#A0A0AA] mt-4 text-sm sm:text-base md:text-lg max-w-3xl">
         My workflow is powered by a carefully chosen set of tools that help me
         ship efficient, maintainable, and elegant apps. Here's a snapshot of my
         core tech stack.
       </p>
-      <div className="grid grid-cols-4 gap-x-10 gap-y-5 mt-15">
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-6 mt-10">
         {skills.map((skill) => (
           <TechCard
             key={skill.name}
